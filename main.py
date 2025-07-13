7b0c7330e50cb22d3695c565a293b32a4f9a6e25
Василий <kulikov1999@gmail.com>
2025-07-14 00:18:26 +0300

    mask_account_card in widget

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-from src import masks
+# from src import masks
+from src import widget
 
 if __name__ == "__main__":
-    print(masks.get_mask_card_number(7000792289606361))
-    print(masks.get_mask_account(73654108430135874305))
+    # print(masks.get_mask_card_number(7000792289606361))
+    # print(masks.get_mask_account(73654108430135874305))
+    print(widget.mask_account_card((
+'Maestro 1596837868705199',
+'MasterCard 7158300734726758',
+'Счет 35383033474447895560',
+'Visa Classic 6831982476737658',
+'Visa Platinum 8990922113665229',
+'Visa Gold 5999414228426353',
+)
+)
+)
