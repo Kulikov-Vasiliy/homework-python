a4c845c55238f988fc83c8751120347116e5a6d3
Василий <kulikov1999@gmail.com>
2025-07-15 17:36:42 +0300

    making edits in mask_account_card

--- main.py
+++ main.py
@@ -4,15 +4,12 @@ from src import widget
 if __name__ == "__main__":
     print(masks.get_mask_card_number(7000792289606361))
     print(masks.get_mask_account(73654108430135874305))
-    print(widget.mask_account_card(
-        (
-'Maestro 1596837868705199',
-'MasterCard 7158300734726758',
-'Счет 35383033474447895560',
-'Visa Classic 6831982476737658',
-'Visa Platinum 8990922113665229',
-'Visa Gold 5999414228426353',
-        )
+    print(widget.mask_account_card('Maestro 1596837868705199,'
+                                   'MasterCard 7158300734726758,'
+                                   'Счет 35383033474447895560,'
+                                   'Visa Classic 6831982476737658,'
+                                   'Visa Platinum 8990922113665229,'
+                                   'Visa Gold 5999414228426353',
     )
 )
     print(widget.get_date('2024-03-11T02:26:18.671407'))
--- src/widget.py
+++ src/widget.py
@@ -1,27 +1,35 @@
 from src.masks import get_mask_card_number, get_mask_account
 
 
-def mask_account_card(confidential_info: tuple) -> str:
+def mask_account_card(confidential_info: str) -> str:
     """Функция, которая обрабатывает информацию о картах и о счетах"""
-    to_mask_info = []
-    info_name = []
-    result = []
-    result_str = []
-    for info_item in confidential_info:
-        splitting_info_item = info_item.split()
-        for part in splitting_info_item:
-            if part.isdigit():
-                to_mask_info.append(part)
-            elif part.isalpha():
-                info_name.append(' '.join(splitting_info_item[:-1]))
-    for el in to_mask_info:
-        if len(el) == 16:
-            result.append(get_mask_card_number(int(el)))
-        elif len(el) == 20:
-            result.append(get_mask_account(int(el)))
-    for i in range(len(result)):
-        result_str.append(f"{info_name[i]} {result[i]}")
-    return "\n".join(result_str)
+    name, number = confidential_info.rsplit(' ', maxsplit=1)
+    if name.lower() == 'счет':
+        masked_number = get_mask_account(int(number))
+    else:
+        masked_number = get_mask_card_number(int(number))
+    return f'{name} {masked_number}'
+
+    # return f'{name} {masked_number}'
+    # to_mask_info = []
+    # info_name = []
+    # result = []
+    # result_str = []
+    # for info_item in confidential_info:
+    #     splitting_info_item = info_item.split()
+    #     for part in splitting_info_item:
+    #         if part.isdigit():
+    #             to_mask_info.append(part)
+    #         elif part.isalpha():
+    #             info_name.append(' '.join(splitting_info_item[:-1]))
+    # for el in to_mask_info:
+    #     if len(el) == 16:
+    #         result.append(get_mask_card_number(int(el)))
+    #     elif len(el) == 20:
+    #         result.append(get_mask_account(int(el)))
+    # for i in range(len(result)):
+    #     result_str.append(f"{info_name[i]} {result[i]}")
+    # return "\n".join(result_str)
 
 
 def get_date(operation_data: str) -> str:
