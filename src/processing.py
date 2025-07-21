a3deafb983ae5f4dd008816ceb823cad5c69cc4c
Василий <kulikov1999@gmail.com>
2025-07-21 21:19:49 +0300

    new branches, module, repository, unsuccessful re-config linters(crashed)

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,11 @@
-def filter_by_state(my_list: list,state='EXECUTED') -> list:
+def filter_by_state(my_list: list, state="EXECUTED") -> list:
     """Функция принимает список словарей и опционально значение для ключа 'state'(по умолчанию 'EXECUTED')
     и возвращает новый список словарей, содержащий только те словари,
     у которых ключ 'state' соответствует указанному значению."""
+    return [el for el in my_list if el["state"] == state]
 
-    return  [el for el in my_list if el["state"] == state]
+
+def sort_by_date(my_list: list, date="2000-01-01T00:00:00.000001") -> list:
+    """ "Принимает список словарей и необязательный параметр, задающий порядок сортировки(по умолчанию — убывание)
+    и возвращать новый список, отсортированный по дате"""
+    return sorted(my_list, key=lambda x: x.get("date", date), reverse=True)
