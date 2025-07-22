42402db150708dd83ff4dde27e52737f772b1fd6
Василий <kulikov1999@gmail.com>
2025-07-22 22:54:04 +0300

    edit sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -5,7 +5,7 @@ def filter_by_state(my_list: list, state="EXECUTED") -> list:
     return [el for el in my_list if el["state"] == state]
 
 
-def sort_by_date(my_list: list, date="2000-01-01T00:00:00.000001") -> list:
+def sort_by_date(my_list: list, ascending=False) -> list:
     """ "Принимает список словарей и необязательный параметр, задающий порядок сортировки(по умолчанию — убывание)
     и возвращать новый список, отсортированный по дате"""
-    return sorted(my_list, key=lambda x: x.get("date", date), reverse=True)
+    return sorted(my_list, key=lambda x: x.get("date"), reverse=True)
