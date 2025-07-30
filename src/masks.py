15ac2ea6743ee37fc82dab1b0721df5715e2da08
Василий <kulikov1999@gmail.com>
2025-07-30 17:09:28 +0300

    deleted extra condition in processing

--- src/masks.py
+++ src/masks.py
@@ -2,7 +2,7 @@ def get_mask_card_number(card_number: str) -> str:
     """Функция маскировки номера банковской карты"""
     if type(card_number) is not str or not card_number.isdigit():
         raise TypeError("некорректный тип данных")
-    elif len(card_number) != 16:
+    if len(card_number) != 16:
         raise ValueError(
             "номер карты должен состоять из 16 символов" " или нет открытых продуктов"
         )
@@ -17,9 +17,8 @@ def get_mask_account(client_account: str) -> str:
     """Функция маскировки номера банковского счета"""
     if type(client_account) is not str or not client_account.isdigit():
         raise TypeError("некорректный тип данных или нет открытых счетов")
+    if len(client_account) != 20:
+        raise ValueError("банковский счет должен состоять из 20 символов")
     else:
-        if len(client_account) != 20:
-            raise ValueError("банковский счет должен состоять из 20 символов")
-        else:
-            late_num = client_account[-4:]
+        late_num = client_account[-4:]
     return f"** {late_num}"
--- src/widget.py
+++ src/widget.py
@@ -3,40 +3,37 @@ from src.masks import get_mask_account, get_mask_card_number
 
 def mask_account_card(confidential_info: str) -> str:
     """Функция, которая обрабатывает информацию о картах и о счетах"""
-    if confidential_info is None:
-        raise TypeError("нет открытых продуктов или счетов")
-    elif confidential_info.count(" ") == 0:
+    if confidential_info.count(" ") == 0:
         raise ValueError("некорректный формат продукта или счета")
     else:
-        spaceless_name = ""
         name, number = confidential_info.rsplit(" ", maxsplit=1)
+        spaceless_name = ""
         if " " in name:
             spaceless_name += name.replace(" ", "")
         elif " " not in name:
             spaceless_name += name
-        elif not spaceless_name.isalpha():
-            raise ValueError("некорректный формат продукта или счета")
+            if not spaceless_name.isalpha():
+                raise ValueError("некорректный формат продукта или счета")
 
-        if not number.isdigit():
-            raise ValueError("некорректный формат продукта или счета")
-        elif name.lower() == "счет":
+        if name.lower() == "счет":
             masked_number = get_mask_account(number)
         else:
             masked_number = get_mask_card_number(number)
+
     return f"{name} {masked_number}"
 
 
 def get_date(operation_data: str) -> str:
     """Принимает на вход строку с датой в формате '2024-03-11T02:26:18.671407'
     и возвращает строку с датой в формате 'ДД.ММ.ГГГГ'"""
-    if operation_data is None:
-        raise TypeError("нет операций")
-    elif "T" not in operation_data:
+    if "T" not in operation_data:
         raise ValueError("некорректный формат даты")
+
     else:
         date_str, _ = operation_data.split("T")
         if date_str.count("-") != 2:
             raise ValueError("некорректный формат даты")
         else:
             year, month, day = date_str.split("-")
+
     return f"{day}.{month}.{year}"
