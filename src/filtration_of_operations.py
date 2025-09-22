77680441cb5b5d8b15559e07bef2613bb36ec009
Василий <kulikov821999@gmail.com>
2025-09-22 22:56:09 +0300

    reinstall git

---
+++ src/filtration_of_operations.py
@@ -0,0 +1,51 @@
+import re
+
+import os
+
+import logging
+
+from src.utils import operations_filled
+
+from src.tables_reader import read_csv, read_excel
+
+BASE_DIR = os.path.dirname(__file__)
+DATA_PATH_JSON = os.path.join(BASE_DIR, "..", "data", "operations.json")
+DATA_PATH_CSV = os.path.join(BASE_DIR, "..", "data", "transactions.csv")
+DATA_PATH_XLSX = os.path.join(BASE_DIR, "..", "data", "transactions_excel.xlsx")
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
+
+
+def process_bank_search(data:list[dict], search:str)->list[dict]:
+    """Функция принимает список словарей с данными о банковских операциях и строку поиска,
+    и возвращает список словарей, у которых в описании есть данная строка."""
+    try:
+        re.escape(search)
+        result = []
+
+        for operation in data:
+            for search in operation:
+                desc = operation.get("description")
+                if search in desc:
+                   wanted_desc = re.findall(search, desc, re.IGNORECASE)
+                   print(type(wanted_desc))
+                   result.append(operation)
+        return result
+
+    except Exception as er:
+        print(er)
+
+
+def process_bank_operations(data:list[dict], categories:list)->dict:
+    """Функция принимает список словарей с данными о банковских операциях и список категорий операций,
+    и возвращает словарь, в котором ключи — это названия категорий,
+    а значения — это количество операций в каждой категории.
+    (Категории операций хранятся в поле description.)"""
+    pass
+
+
+if __name__ == "__main__":
+    print(process_bank_search(data=operations_filled, search=input().lower()),logging.NullHandler())
+    print(process_bank_search(data=read_csv(path_file=DATA_PATH_CSV), search=input().lower()))
+    print(process_bank_search(data=read_excel(path_file=DATA_PATH_XLSX), search=input().lower()))
